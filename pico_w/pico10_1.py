7d9647266efc5f5430cb8548ea7d9a46bb25484b
jackalcha <jackalcha@gmail.com>
2023-12-16 15:18:49 +0800

    modify

--- pico_w/pico10_1.py
+++ pico_w/pico10_1.py
@@ -16,8 +16,8 @@ def callback1(t:Timer):
     #溫度超過24度,並且發送alert()的時間大於60秒
     if temperature >= 24 and delta >= 60 * 1000:
         alert()
-        start = time.ticks_ms()
+        start = time.ticks_ms()#重新設定計時的時間
         
-start = time.ticks_ms() - 60 * 1000   
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒
 time1 = Timer()
 time1.init(period=1000,callback=callback1)
