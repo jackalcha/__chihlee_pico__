4b89bdd26a715bd51500f7bf6bf0e73e4fff1a11
jackalcha <jackalcha@gmail.com>
2023-12-16 12:02:20 +0800

    modify

--- pico_w/pico9_1.py
+++ pico_w/pico9_1.py
@@ -3,9 +3,10 @@ from machine import Pin
 led = Pin("LED",Pin.OUT)
 
 while True:
-    led.on()
-    time.sleep_ms(300)
-    led.off()
-    time.sleep_ms(300)
+    led.high()
+    time.sleep_ms(100)
+    led.low()
+    time.sleep_ms(100)
+    
     
     
