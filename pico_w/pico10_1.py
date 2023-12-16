3dfce858e2c4b9153c4f808b5982a03f7d887cb5
jackalcha <jackalcha@gmail.com>
2023-12-16 14:09:03 +0800

    modify

---
+++ pico_w/pico10_1.py
@@ -0,0 +1,10 @@
+from machine import ADC,Timer
+
+def callback1(t:Timer):
+    sensor = ADC(4)
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706) / 0.001721
+    print(temperature)
+    
+time1 = Timer()
+time1.init(period=1000,callback=callback1)
--- pico_w/pico9_1.py
+++ pico_w/pico9_1.py
@@ -1,12 +1,8 @@
 import time
 from machine import Pin
 led = Pin("LED",Pin.OUT)
-
 while True:
     led.high()
     time.sleep_ms(100)
     led.low()
     time.sleep_ms(100)
-    
-    
-    
