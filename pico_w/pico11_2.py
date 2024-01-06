aeab48676569932010a0ab27988a4e73ddd611ea
jackalcha <jackalcha@gmail.com>
2024-01-06 12:27:31 +0800

    Update pico11_2.py

--- pico_w/pico11_2.py
+++ pico_w/pico11_2.py
@@ -1,6 +1,6 @@
 import network
 import time
-from machine import WDT,Timer
+from machine import WDT,Timer,ADC
 
 def connect():
     # enable station interface and connect to WiFi access point
@@ -32,13 +32,29 @@ def connect():
         print("成功連線")
         print(nic.ifconfig())
 
-def mycallback(t):
-    print("Hello! World")
-    #t.deinit()
+
+
+
+def alert():
+    print('要爆炸了!')
+    
+def callback1(t:Timer):
+    global start
+    sensor = ADC(4)
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706) / 0.001721
+    print(f'溫度:{temperature}')
+    delta = time.ticks_diff(time.ticks_ms(), start)
+    print(delta)
+    #溫度超過24度,並且發送alert()的時間大於60秒
+    if temperature >= 24 and delta >= 60 * 1000:
+        alert()
+        start = time.ticks_ms()#重新設定計時的時間
 
 connect()
-tim= Timer()
-tim.init(period=1000,callback=mycallback)
 
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒
+time1 = Timer()
+time1.init(period=1000,callback=callback1)
 
 
