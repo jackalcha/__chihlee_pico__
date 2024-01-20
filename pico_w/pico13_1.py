d5fcda6ecf0e540c3ab4f982139ab35dcc06342e
jackalcha <jackalcha@gmail.com>
2024-01-20 12:15:37 +0800

    Update pico13_1.py

--- pico_w/pico13_1.py
+++ pico_w/pico13_1.py
@@ -2,13 +2,15 @@ from machine import Pin
 import time
 red_led = Pin(15,mode=Pin.OUT)
 btn = Pin(14,mode=Pin.PULL_DOWN)
+is_press = False
 
 while True:
-    print(btn.value())
-    time.sleep_ms(500)
-    if btn.value() == 1 :
+    if btn.value():
+        is_press = True
         red_led(1)
-    else:
+    elif is_press :
+        print('release')
+        is_press = False
         red_led(0)
     
     
