d3f065a7a63285a55976bdafe6653cae284c1a09
jackalcha <124478041+jackalcha@users.noreply.github.com>
2024-02-03 06:50:06 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -1,10 +1,14 @@
 import requests
 import streamlit as st
 import pandas as pd
+from streamlit_autorefresh import st_autorefresh
+
+st_autorefresh(interval=5000)
 
 st.title('Pico_W 專案')
 st.header('雞舍:red[溫度]和:blue[光線]狀態')
 st.divider()
+st.snow()
 
 url = 'https://blynk.cloud/external/api/get?token=_OgXaLhGv-t1s-Gw8j5I5Sr_5b-2zUov&v1&v0'
 
